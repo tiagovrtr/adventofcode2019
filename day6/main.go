82774e2c383ad2f22466aea015a162e66b290da5
Tiago Reis <tiagovrtreis@gmail.com>
2019-12-09 23:40:45 +0000

    Day 6 Part 2

--- day6/main.go
+++ day6/main.go
@@ -10,22 +10,55 @@ import (
 
 type OrbitNode struct {
 	current string
+	prev    *OrbitNode
 	next    []*OrbitNode
 }
 
 type Orbit map[string]*OrbitNode
 
+type Set map[string]bool
+
 func (tree *Orbit) add(prev, curr string) {
-	if (*tree)[curr] == nil {
-		var newcurrnode = OrbitNode{curr, nil}
-		(*tree)[curr] = &newcurrnode
-	}
-	if (*tree)[prev] == nil {
-		var newprevnode = OrbitNode{prev, nil}
-		(*tree)[prev] = &newprevnode
-	}
+	var prevnode *OrbitNode = (*tree)[prev]
 	var currnode *OrbitNode = (*tree)[curr]
-	(*tree)[prev].next = append((*tree)[prev].next, currnode)
+	if currnode == nil {
+		var newcurrnode = OrbitNode{curr, nil, nil}
+		(*tree)[curr] = &newcurrnode
+		currnode = &newcurrnode
+	}
+	if prevnode == nil {
+		var newprevnode = OrbitNode{prev, nil, nil}
+		(*tree)[prev] = &newprevnode
+		prevnode = &newprevnode
+	}
+	prevnode.next = append(prevnode.next, currnode)
+	currnode.prev = prevnode
+}
+
+func (tree *Orbit) shortestPath(src, dst string) int {
+	var srcnode *OrbitNode = (*tree)[src]
+	var visited = Set{src: true}
+	distance := 0
+	return srcnode.shortestPath(dst, distance, &visited)
+
+}
+
+func (node *OrbitNode) shortestPath(dst string, distance int, visited *Set) int {
+	(*visited)[(*node).current] = true
+	if (*node).current == dst {
+		return distance
+	} else {
+		neighb := append((*node).next, (*node).prev)
+		for _, next := range neighb {
+			if _, found := (*visited)[(*next).current]; !found {
+				newdist := next.shortestPath(dst, distance+1, visited)
+				if newdist != -1 {
+					return newdist
+				}
+			}
+		}
+	}
+	return -1
 }
 
 func (node *OrbitNode) checksum(depth int) int {
@@ -57,4 +90,5 @@ func main() {
 	}
 
 	fmt.Println(tree["COM"].checksum(0))
+	fmt.Println(tree.shortestPath("YOU", "SAN") - 2)
 }
